492f6b271948a6bb9f67d91962b81ddfc5911d33
Philipp Albrecht <philippbalbrecht@gmail.com>
2018-08-14 20:09:03 -0700

    Getting live data

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,23 +1,23 @@
 
 var searchYouTube = (options, callback) => {
   $.ajax({
-    method: "GET",
+    method: 'GET',
     url: 'https://www.googleapis.com/youtube/v3/search',
     data: {
-           maxResults: options.max,
-           key: options.key,
-           part: 'snippet',
-           q: options.query,
-           type: 'video',
-           videoEmbeddable: true
-        },
+      maxResults: options.max,
+      key: options.key,
+      part: 'snippet',
+      q: options.query,
+      type: 'video',
+      videoEmbeddable: true
+    },
     success: function(data) {
       callback(data.items);
     },
-    error: function(data){
+    error: function(data) {
       console.log('Failed to get videos');
     }
-  })
+  });
 };
 
 
