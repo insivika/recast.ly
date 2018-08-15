71daf0f531036bd6e450d967f88430024868cb13
Philipp Albrecht <philippbalbrecht@gmail.com>
2018-08-14 20:07:54 -0700

    Getting live data from Youtube

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -20,7 +21,7 @@ class App extends React.Component {
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
+            <Search />
           </div>
         </nav>
         <div className="row">
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,24 @@
+
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    method: "GET",
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+           maxResults: options.max,
+           key: options.key,
+           part: 'snippet',
+           q: options.query,
+           type: 'video',
+           videoEmbeddable: true
+        },
+    success: function(data) {
+      callback(data.items);
+    },
+    error: function(data){
+      console.log('Failed to get videos');
+    }
+  })
 };
+
 
 window.searchYouTube = searchYouTube;
